711d63e74fb587425bae1619756de34c19b8854e
Wonders Victor <wondersprince1@gmail.com>
2024-02-24 23:14:13 +0100

    Pycodestyle fixes

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,27 +1,30 @@
 #!/usr/bin/python3
+'''Index API handler'''
+import json
 from api.v1.views import app_views
 from flask import Response
 from models import storage
-import json
+
 
 @app_views.route("/status", methods=["GET"])
 def status():
-	'''Returns app status'''
-	status = json.dumps({"status": "OK"}, indent=4)
-	return Response(status + '\n', status=200, mimetype="application/json")
+    '''Returns app status'''
+    status = json.dumps({"status": "OK"}, indent=4)
+    return Response(status + '\n', status=200, mimetype="application/json")
+
 
 @app_views.route("/stats", methods=["GET"])
 def stats():
-	'''Returns stats'''
-	names = { 
-		"Amenity": "amenities",
-		"City": "cities",
-		"Place": "places",
-		"Review": "reviews",
-		"State": "states",
-		"User": "users"}
-	stats = {}
-	for key in names.keys():
-		stats[names[key]] = storage.count(key)
-		data = json.dumps(stats, indent=4)
-	return Response(data + '\n', status=200, mimetype="application/json")
+    '''Returns stats'''
+    names = {
+        "Amenity": "amenities",
+        "City": "cities",
+        "Place": "places",
+        "Review": "reviews",
+        "State": "states",
+        "User": "users"}
+    stats = {}
+    for key in names.keys():
+        stats[names[key]] = storage.count(key)
+        data = json.dumps(stats, indent=4)
+    return Response(data + '\n', status=200, mimetype="application/json")
