cab94e7227e045c40025b8326cb22336f204be53
Wonders Victor <wondersprince1@gmail.com>
2024-02-26 00:48:17 +0100

    Task 12

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,22 +6,31 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 from flask import Response
+from flask_cors import CORS
 
 
 app = Flask(__name__)
 
 
+# Enable CORS on all routes
+cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
+
+# Register blueprint
 app.register_blueprint(app_views)
 
 
+# Teardown function to close storage
 @app.teardown_appcontext
 def teardown(exception):
     '''Close Storage'''
     storage.close()
 
 
+# Error handle for 404 response
 @app.errorhandler(404)
 def not_found(error):
+    '''Handles Error 404'''
     return Response(json.dumps({'error': 'Not found'}, indent=4) + '\n', 404)
 
 
