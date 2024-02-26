ed6f36413823e3fd0d4f1941f52da779c7dc5d3e
Wonders Victor <wondersprince1@gmail.com>
2024-02-26 22:34:52 +0000

    Testing

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -8,7 +8,7 @@ from models.place import Place
 from models.amenity import Amenity
 import json
 from api.v1.views import app_views
-from flask import Response, abort, request
+from flask import Response, abort, request, jsonify
 from models import storage
 
 
@@ -29,51 +29,61 @@ def get_place_amenities(place_id):
 	return Response(amenity_data + '\n', status=200, mimetype="application/json")
 
 
-@app_views.route('/places/<place_id>/amenities/<amenity_id>',
-                 methods=['DELETE'], strict_slashes=False)
-def get_amenity(place_id, amenity_id):
-	'''Retrieves a Amenity object'''
-	state = storage.get(State, state_id)
-	if not state:
-		abort(404)
-	state_data = json.dumps(state.to_dict(), indent=4)
-	return Response(state_data + '\n', status=200, mimetype="application/json")
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+                 strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """
+    Deletes a Amenity object of a Place
+    """
+    place = storage.get(Place, place_id)
 
-@app_views.route("/states/<state_id>", methods=["DELETE"], strict_slashes=False)
-def delete_state(state_id):
-	'''Deletes a state object'''
-	state = storage.get(State, state_id)
-	if not state:
-		abort(404)
-	storage.delete(state)
-	storage.save()
-	return Response(json.dumps({}), 200, mimetype="application/json")
+    if not place:
+        abort(404)
 
-@app_views.route("/states", methods=["POST"], strict_slashes=False)
-def create_state():
-	'''Creates a State'''
-	if not request.json:
-		abort(400, "Not a JSON")
-	if "name" not in request.json:
-		abort(400, "Missing Name")
-	data = request.get_json()
-	new_state = State(**data)
-	new_state.save()
-	return Response(json.dumps(new_state.to_dict(), indent=4) + '\n', 201,
-				    mimetype="application/json")
+    amenity = storage.get(Amenity, amenity_id)
 
-app_views.route("/states/<state_id>", methods=["PUT"], strict_slashes=False)
-def update_state(state_id):
-	'''Updates a state object'''
-	if not request.json:
-		abort(400, "Not a JSON")
-	state = storage.get(State, state_id)
-	if not state:
-		abort(404)
-	data = request.get_json()
-	for key, value in data.items():
-		if key not in ["id", "created_at", "updated_at"]:
-			setattr(state, key, value)
-	state.save()
-	return Response(json.dumps(state.to_dict(), indent=4) + '\n', 200,
-				    mimetype="application/json")
+    if not amenity:
+        abort(404)
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+    else:
+        if amenity_id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity_id)
+
+    storage.save()
+    return Response(jsonify({}), 200)
+
+
+@app_views.route('/places/<place_id>/amenities/<amenity_id>', methods=['POST'],
+                 strict_slashes=False)
+def post_place_amenity(place_id, amenity_id):
+    """
+    Link a Amenity object to a Place
+    """
+    place = storage.get(Place, place_id)
+
+    if not place:
+        abort(404)
+
+    amenity = storage.get(Amenity, amenity_id)
+
+    if not amenity:
+        abort(404)
+
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        if amenity in place.amenities:
+            return Response(jsonify(amenity.to_dict()), 200)
+        else:
+            place.amenities.append(amenity)
+    else:
+        if amenity_id in place.amenity_ids:
+            return Response(jsonify(amenity.to_dict()), 200)
+        else:
+            place.amenity_ids.append(amenity_id)
+
+    storage.save()
+    return Response(jsonify(amenity.to_dict()), 201)
