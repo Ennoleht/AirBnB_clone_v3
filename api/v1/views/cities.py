c671e2c530df8d44d83068e69d079a5c0ad6bef7
timmySpark <timmyspark1@gmail.com>
2024-02-27 02:19:18 +0000

    [feat] :- solving pycodestyles error in this file

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 '''Retrieves city objects'''
+
 from models.city import City
 from models.state import State
 import json
@@ -8,68 +9,75 @@ from flask import Response, abort, request
 from models import storage
 
 
-@app_views.route("/states/<state_id>/cities", methods=["GET"], strict_slashes=False)
+@app_views.route("/states/<state_id>/cities",
+                 methods=["GET"], strict_slashes=False)
 def get_cities(state_id):
-	'''Retrieves list of City object of a State'''
-	state = storage.get(State, state_id)
-	if not state:
-		abort(404)
-	city_list = []
-	for key, obj in storage.all(City).items():
-		if obj.state_id == state_id:
-			city_list.append(obj.to_dict())
-	city_data = json.dumps(city_list, indent=4)
-	return Response(city_data + '\n', status=200, mimetype="application/json")
+    '''Retrieves list of City object of a State'''
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    city_list = []
+    for key, obj in storage.all(City).items():
+        if obj.state_id == state_id:
+            city_list.append(obj.to_dict())
+    city_data = json.dumps(city_list, indent=4)
+    return Response(city_data + '\n', status=200, mimetype="application/json")
 
 
 @app_views.route("/cities/<city_id>", methods=["GET"], strict_slashes=False)
 def get_city(city_id):
-	'''Retrieves a City object'''
-	city = storage.get(City, city_id)
-	if not city:
-		abort(404)
-	city_data = json.dumps(city.to_dict(), indent=4)
-	return Response(city_data + '\n', status=200, mimetype="application/json")
+    '''Retrieves a City object'''
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    city_data = json.dumps(city.to_dict(), indent=4)
+    return Response(city_data + '\n', status=200, mimetype="application/json")
+
 
 @app_views.route("/cities/<city_id>", methods=["DELETE"], strict_slashes=False)
 def delete_city(city_id):
-	'''Deletes a city object'''
-	city = storage.get(City, city_id)
-	if not city:
-		abort(404)
-	storage.delete(city)
-	storage.save()
-	return Response(json.dumps({}), 200, mimetype="application/json")
+    '''Deletes a city object'''
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+    storage.delete(city)
+    storage.save()
+    return Response(json.dumps({}), 200, mimetype="application/json")
 
-@app_views.route("/states/<state_id>/cities", methods=["POST"], strict_slashes=False)
+
+@app_views.route("/states/<state_id>/cities",
+                 methods=["POST"], strict_slashes=False)
 def create_city(state_id):
-	'''Creates a City'''
-	if not request.json:
-		abort(400, "Not a JSON")
-	state = storage.get(State, state_id)
-	if not state:
-		abort(404)
-	if "name" not in request.json:
-		abort(400, "Missing Name")
-	data = request.get_json()
-	new_city = City(**data)
-	setattr(new_city, "state_id", state_id)
-	new_city.save()
-	return Response(json.dumps(new_city.to_dict(), indent=4) + '\n', 201,
-				    mimetype="application/json")
+    '''Creates a City'''
+    if not request.json:
+        abort(400, "Not a JSON")
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+    if "name" not in request.json:
+        abort(400, "Missing Name")
+    data = request.get_json()
+    new_city = City(**data)
+    setattr(new_city, "state_id", state_id)
+    new_city.save()
+    return Response(json.dumps(new_city.to_dict(), indent=4) + '\n', 201,
+                    mimetype="application/json")
 
-app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
+
+@app_views.route("/cities/<city_id>", methods=["PUT"], strict_slashes=False)
 def update_city(city_id):
-	'''Updates a city object'''
-	if not request.json:
-		abort(400, "Not a JSON")
-	city = storage.get(City, city_id)
-	if not city:
-		abort(404)
-	data = request.get_json()
-	for key, value in data.items():
-		if key not in ["id", "created_at", "updated_at"]:
-			setattr(city, key, value)
-	city.save()
-	return Response(json.dumps(city.to_dict(), indent=4) + '\n', 200,
-				    mimetype="application/json")
+    '''Updates a city object'''
+    if not request.json:
+        abort(400, "Not a JSON")
+
+    city = storage.get(City, city_id)
+    if not city:
+        abort(404)
+
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(city, key, value)
+
+    city.save()
+    return jsonify(city.to_dict()), 200
