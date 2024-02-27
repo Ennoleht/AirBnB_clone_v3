212f60ef595b931e7734bc0abcf6a4f3db0c51dc
timmySpark <timmyspark1@gmail.com>
2024-02-27 02:26:22 +0000

    [feat] :- solving pycodestyle errors and modifying put method codes

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -5,7 +5,7 @@ from models.city import City
 from models.state import State
 import json
 from api.v1.views import app_views
-from flask import Response, abort, request
+from flask import Response, abort, request, jsonify
 from models import storage
 
 
